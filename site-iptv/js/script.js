c4fcb7a12f7cd738787967c0796456c83569fae5
GugaSi21 <88911825+GugaSi21@users.noreply.github.com>
2024-12-30 22:58:56 -0400

    Update script.js

--- site-iptv/js/script.js
+++ site-iptv/js/script.js
@@ -1,5 +1,5 @@
 function openWhatsApp() {
-    var phoneNumber = "5569993073838"; // Substitua com o seu número de WhatsApp
+    var phoneNumber = "5566992634123"; // Substitua com o seu número de WhatsApp
     var message = encodeURIComponent("Olá! Gostaria de saber mais sobre os planos de IPTV.");
     
     var whatsappUrl = `https://wa.me/${phoneNumber}?text=${message}`;
